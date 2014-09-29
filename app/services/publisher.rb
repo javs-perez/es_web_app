ab904481b7699d0a4fb3893dc5b280c5b983d246
Javier Perez <perez.javi.a@gmail.com>
2014-09-29 14:39:56 -0400

    added test for notification

--- app/services/publisher.rb
+++ app/services/publisher.rb
@@ -1,8 +1,15 @@
 class Publisher
-	def self.publish(exchange, message = {})
-		x = channel.fanout("project.#{exchange}")
+	def initialize(connection, exchange, channel)
+    @connection = self.class.connection
+    @channel = self.class.create_channel(@session)
+    @exchange = self.class.publish(@channel)
+  end
 
-		x.publish(message.to_json, :durable => true)
+
+	def self.publish(exchange, message = {})
+		@exchange = channel.fanout("project.#{exchange}")
+
+		@exchange.publish(message.to_json, :durable => true)
 	end
 
 	def self.channel
--- spec/controllers/test_spec.rb
+++ spec/controllers/test_spec.rb
@@ -5,7 +5,7 @@ describe ProjectsController do
 		@user = User.create(name: "test dummy", phone: "305-555-5555", email: "something@something.com")
 	end
 
-	it "should create a project and change status" do 
+	it "should create a project and notify" do 
 		post :create, { project: {user_id: @user.id, status: 'Approved', name: "Test Project"} }
 		expect(response.status).to eq(302)
 		expect(assigns(:project).name).to eq("Test Project")
@@ -15,5 +15,13 @@ describe ProjectsController do
 		post :update, { id: project_id, project: { status: 'Funding' } }
 		expect(response.status).to eq(302)	
 		expect(assigns(:project).status).to eq("Funding")
+	
+		
+		connection = 	double('connection')
+		channel = 		double('channel')
+		exchange = 		double('exchange')
+		Publisher.stub(:connection) { connection }
+		Publisher.stub(:create_channel) { channel }
+		Publisher.stub(:publish) { exchange } 
 	end
 end
